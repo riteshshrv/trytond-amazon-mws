f0d2b6ce7f9b083ec2a6e44983ec10f553ed06b1
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2015-10-07 18:12:40 -0500

    Refactor export method for multi-channel

--- shipment.py
+++ shipment.py
@@ -2,6 +2,7 @@
 """
     shipment.py
 """
+from collections import defaultdict
 from lxml.builder import E
 from lxml import etree
 from trytond.pool import PoolMeta, Pool
@@ -18,38 +19,53 @@ class ShipmentOut:
     def export_shipment_status_to_amazon(self):
         SaleLine = Pool().get('sale.line')
 
-        if not self.sales or self.state != 'done':
+        if self.state != 'done':
             return
 
-        sale = self.sales[0]
+        # Handle the case where a shipment could have been merged
+        # across channels or even two amazon accounts.
+        items_by_sale = defaultdict(list)
+
+        # Fulfilment data remains the same
         fulfilment_data = E.FulfillmentData(
             E.CarrierCode(self.carrier.carrier_cost_method),
             E.ShippingMethod(self.carrier.carrier_cost_method),
             E.ShipperTrackingNumber(self.tracking_number),
         )
+
         for move in self.outgoing_moves:
+            if not move.quantity:
+                # back order
+                continue
             if not isinstance(move.origin, SaleLine):
                 continue
-            fulfilment_data.append(E.Item(
-                E.AmazonOrderItemCode(move.origin.channel_identifier),
-                E.MerchantFulfillmentItemID(str(move.id)),
-                E.Quantity(str(move.quantity))
-            ))
-        message = E.Message(
-            E.MessageID(str(self.id)),
-            E.OrderFulfillment(
-                E.AmazonOrderID(sale.reference),
-                E.MerchantFulfillmentID(self.code),
-                E.FulfillmentDate(str(self.effective_date.isoformat())),
-                fulfilment_data
+            if move.origin.channel.source != 'amazon_mws':
+                continue
+            items_by_sale[move.origin.sale].append(
+                E.Item(
+                    E.AmazonOrderItemCode(move.origin.channel_identifier),
+                    E.MerchantFulfillmentItemID(str(move.id)),
+                    E.Quantity(str(move.quantity))
+                )
             )
-        )
-        envelope_xml = sale.channel._get_amazon_envelop(
-            'OrderFulfillment', message
-        )
-        feeds_api = sale.channel.get_amazon_feed_api()
-        feeds_api.submit_feed(
-            etree.tostring(envelope_xml),
-            feed_type='_POST_ORDER_FULFILLMENT_DATA_',
-            marketplaceids=[sale.channel.amazon_marketplace_id]
-        )
+
+        for sale, items in items_by_sale.items():
+            message = E.Message(
+                E.MessageID(str(sale.id)),  # just has to be unique in envelope
+                E.OrderFulfillment(
+                    E.AmazonOrderID(sale.channel_identifier),
+                    E.MerchantFulfillmentID(self.code),
+                    E.FulfillmentDate(str(self.effective_date.isoformat())),
+                    fulfilment_data,
+                    *items
+                )
+            )
+            envelope_xml = sale.channel._get_amazon_envelop(
+                'OrderFulfillment', [message]
+            )
+            feeds_api = sale.channel.get_amazon_feed_api()
+            feeds_api.submit_feed(
+                etree.tostring(envelope_xml),
+                feed_type='_POST_ORDER_FULFILLMENT_DATA_',
+                marketplaceids=[sale.channel.amazon_marketplace_id]
+            )
